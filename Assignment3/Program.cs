db76d68770bd89fd4355fd9c8be551322f9197a6
TheEndurance <rawa.jalal@gmail.com>
2018-03-26 12:32:45 -0400

    program complete and timeconversion complete

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -8,8 +8,89 @@ namespace Assignment3
 {
     class Program
     {
-        static void Main(string[] args)
+        static int PrintMenu()
         {
+            string userInput = string.Empty;
+            int menuSelection = -1;
+            Console.Clear();
+            Console.WriteLine("1. Convert time");
+            Console.WriteLine("2. Exit");
+
+            userInput = Console.ReadLine();
+            try
+            {
+                menuSelection = int.Parse(userInput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return menuSelection;
+        }
+
+        static double GetDateTime()
+        {
+            string userInput = string.Empty;
+            double time = 0.00;
+
+            while (time <= 0.00)
+            {
+                Console.WriteLine($"Enter the value of the time");
+                userInput = Console.ReadLine();
+                try
+                {
+                    time = Double.Parse(userInput);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occured : {ex.Message}");
+                }
+            }
+            return time;
+        }
+
+        static string[] GetConvertStrings()
+        {
+            string[] convertStrings = new string[2];
+            Console.WriteLine("Enter the unit of the time you entered above");
+            convertStrings[0] = Console.ReadLine();
+            Console.WriteLine("Enter the unit of time you wish to convert to");
+            convertStrings[1] = Console.ReadLine();
+            return convertStrings;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            bool exited = false;
+
+            while (!exited)
+            {
+                int menuSelected = PrintMenu();
+                while (menuSelected == -1)
+                {
+                    menuSelected = PrintMenu();
+                }
+
+                switch (menuSelected)
+                {
+                    case 1:
+                        double time = GetDateTime();
+                        string[] dateTimeStrings = GetConvertStrings();
+                        double result = TimeConversion.Convert(time, dateTimeStrings[0], dateTimeStrings[1]);
+                        Console.WriteLine($"The time of {time} {dateTimeStrings[0]} converted to {dateTimeStrings[1]} is {result} {dateTimeStrings[1]}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Thank you, exiting now...");
+                        exited = true;
+                        break;
+                }
+                if (!exited)
+                {
+                    Console.WriteLine("press any key to continue to menu");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
---
+++ Assignment3/TimeConversion.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public static class TimeConversion
+    {
+        private static readonly string[] AllowedTimeStringForSeconds = {"seconds", "Seconds", "s", "S"};
+        private static readonly string[] AllowedTimeStringForMinutes = {"minutes", "Minutes", "m", "M"};
+        private static readonly string[] AllowedTimeStringForHours = {"hours", "Hours", "h", "H"};
+        private static readonly string[] AllowedTimeStringForDays = {"days", "Days", "d", "D"};
+
+        public static double Convert(double value, string convertFrom, string convertTo)
+        {
+            string modifiedConvertFrom = ModifyInput(convertFrom);
+            string modifiedConvertTo = ModifyInput(convertTo);
+            return value * GetMultiplier(modifiedConvertFrom,modifiedConvertTo);
+        }
+
+        private static string ModifyInput(string input)
+        {
+            string output = string.Empty;
+            foreach (string dateTimeString in AllowedTimeStringForSeconds)
+            {
+                if (input.Equals(dateTimeString))
+                {
+                    output = "seconds";
+                }
+            }
+            foreach (string dateTimeString in AllowedTimeStringForMinutes)
+            {
+                if (input.Equals(dateTimeString))
+                {
+                    output = "minutes";
+                }
+            }
+            foreach (string dateTimeString in AllowedTimeStringForHours)
+            {
+                if (input.Equals(dateTimeString))
+                {
+                    output = "hours";
+                }
+            }
+            foreach (string dateTimeString in AllowedTimeStringForDays)
+            {
+                if (input.Equals(dateTimeString))
+                {
+                    output = "days";
+                }
+            }
+            return output;
+        }
+
+        private static double GetMultiplier(string convertFrom, string convertTo)
+        {
+            double convertFromValue = 0.00;
+            double convertToValue = 0.00;
+
+            switch (convertFrom)
+            {
+                case "seconds":
+                    convertFromValue = 1;
+                    break;
+                case "minutes":
+                    convertFromValue = 60;
+                    break;
+                case "hours":
+                    convertFromValue = 3600;
+                    break;
+                case "days":
+                    convertFromValue = 86400;
+                    break;
+            }
+
+            switch (convertTo)
+            {
+                case "seconds":
+                    convertToValue = 1;
+                    break;
+                case "minutes":
+                    convertToValue = 60;
+                    break;
+                case "hours":
+                    convertToValue = 3600;
+                    break;
+                case "days":
+                    convertToValue = 86400;
+                    break;
+            }
+
+            return convertFromValue / convertToValue;
+
+        }
+    }
+}
